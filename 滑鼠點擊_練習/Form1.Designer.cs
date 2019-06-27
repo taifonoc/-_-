5ec139c870a321076d7b56ed8f6d5d89aa4d2658
王昱欽 <wizozc38122@outlook.com>
2019-06-27 15:15:31 +0800

    加入專案檔案。

---
+++ 滑鼠點擊_練習/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace 滑鼠點擊_練習
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lebal = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lebal
+            // 
+            this.lebal.AutoSize = true;
+            this.lebal.Font = new System.Drawing.Font("新細明體", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lebal.Location = new System.Drawing.Point(68, 55);
+            this.lebal.Name = "lebal";
+            this.lebal.Size = new System.Drawing.Size(194, 27);
+            this.lebal.TabIndex = 0;
+            this.lebal.Text = "你已經按了 0下";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(99, 158);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "點我";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(99, 242);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 47);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "重新";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 329);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lebal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lebal;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ 滑鼠點擊_練習/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 滑鼠點擊_練習
+{
+    public partial class Form1 : Form
+    {
+        int times = 0;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            times++;
+            lebal.Text = "你已經按了 " + times + "下";
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            times = 0;
+            lebal.Text = "你已經按了 0下";
+        }
+    }
+}
